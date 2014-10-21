e135fcfecdb3075ffb0b3c561834aeac94680803
Abid ANIS <anab.tn@gmail.com>
2014-10-21 13:20:23 +0100

    init project

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    /*
+     * init browser
+     */
+    init();
+
+    /*
+     * init resize browser
+     */
+    $(window).resize(function() {
+        init();
+    });
+
+});
+
+/*
+ * init function
+ */
+
+function init() {
+	// Code init function
+}
